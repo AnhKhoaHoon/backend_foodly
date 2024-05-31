a1d864de3f27573faa1b190fab3c10e7ffba52a8
John Doe <johndoe@example.com>
2024-05-31 11:19:20 +0700

    Creating DB Cluster and Extraction the URI

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const express =require('express')
+const express = require('express')
 const app = express()
-const port = 3000
-app.get('/',(req, res) => res.send('Hello Word!'))
-app.listen(port, () => console.log('Foddly Backend is running on ${port}!'))
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGOURL)
+.then(() => console.log("Foodly Database Connected"))
+.catch((err) => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(process.env.PORT || 6013, () => console.log(`Foodly Backend is running on ${process.env.PORT}!`))
