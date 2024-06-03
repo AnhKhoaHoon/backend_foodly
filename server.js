330914f058dab54015e642bd86458bb474ef1393
John Doe <johndoe@example.com>
2024-06-03 10:11:33 +0700

    Restaurant Functions

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const CategoryRoute = require("./routes/category")
+const RestaurantRoute = require("./routes/restaurant")
 
 dotenv.config();
 
@@ -10,8 +11,11 @@ mongoose.connect(process.env.MONGOURL)
 .then(() => console.log("Foodly Database Connected"))
 .catch((err) => console.log(err));
 
-app.use(express.json());
+app.use(express.json());                 
 app.use(express.urlencoded({extended: true}));
-app.use("/api/category",CategoryRoute);
+app.use("/api/category", CategoryRoute);
+app.use("/api/restaurant", RestaurantRoute);
+
+
 
 app.listen(process.env.PORT || 6013, () => console.log(`Foodly Backend is running on ${process.env.PORT}!`))
